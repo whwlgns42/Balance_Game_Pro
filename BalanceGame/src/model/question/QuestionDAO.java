7e5e62668c62729a987739732af3a3627be52313
조지훈 <whwlgns42@kakao.com>
2023-12-30 15:31:50 +0900

    QuestionDao 수정

--- BalanceGame/src/model/question/QuestionDAO.java
+++ BalanceGame/src/model/question/QuestionDAO.java
@@ -17,10 +17,9 @@ public class QuestionDAO {
 	private static final String SELECTONE = "SELECT * FROM (SELECT * FROM QUESTIONS ORDER BY DBMS_RANDOM.VALUE) WHERE ROWNUM = 1";
 	private static final String SELECT_PK_ONE = "SELECT * FROM QUESTIONS WHERE IDX = ?";
 	private static final String INSERT = "INSERT INTO QUESTIONS(IDX, TITLE, CONTENT_A, CONTENT_B, WRITER)  VALUES((SELECT NVL(MAX(IDX), 0) + 1 FROM QUESTIONS),?,?,?,?)";
-	private static final String UPDATE = "";
-	private static final String DELETE = "";
 
-	public ArrayList<QuestionDTO> selectAll(QuestionDTO questionDTO) { // 문제 제목만 전체 조회하기
+	 // 문제 제목만 전체 조회하기
+	public ArrayList<QuestionDTO> selectAll(QuestionDTO questionDTO) {
 		ArrayList<QuestionDTO> datas = new ArrayList<QuestionDTO>();
 		QuestionDTO questionData = null;
 		if (questionDTO.getSearchCondition().equals("문제전체조회")) {
@@ -47,7 +46,8 @@ public class QuestionDAO {
 
 	}
 
-	public QuestionDTO selectOne(QuestionDTO questionDTO) { // 문제생성
+	// 문제생성
+	public QuestionDTO selectOne(QuestionDTO questionDTO) { 
 		QuestionDTO data = null;
 		if (questionDTO.getSearchCondition().equals("문제생성")) {
 			conn = JDBCUtil.connect();
@@ -96,7 +96,7 @@ public class QuestionDAO {
 		return data;
 	} 
 
-	// 크롤링한 문제 추가하기ll
+	// 크롤링한 문제 추가하기
 	public boolean insert(QuestionDTO questionDTO) {
 		conn = JDBCUtil.connect();
 		try {
@@ -120,44 +120,4 @@ public class QuestionDAO {
 		return true;
 	}
 
-//	public boolean update(UserDTO uDTO) { // TODO 미구현
-//		conn = JDBCUtil.connect();
-//		try {
-//			pstmt = conn.prepareStatement(UPDATE);
-//			pstmt.setString(1, uDTO.getId());
-//			pstmt.setString(2, uDTO.getPw());
-//			pstmt.setString(3, uDTO.getName());
-//			pstmt.setString(4, uDTO.getGrade());
-//			pstmt.setString(5, uDTO.getGender());
-//			pstmt.setInt(6, uDTO.getAge());
-//			int result = pstmt.executeUpdate();
-//			if (result <= 0) {
-//				return false;
-//			}
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			return false;
-//		} finally {
-//			JDBCUtil.disconnect(pstmt, conn);
-//		}
-//		return true;
-//	}
-
-//	public boolean delete(UserDTO uDTO) { // TODO 미구현
-//		conn = JDBCUtil.connect();
-//		try {
-//			pstmt = conn.prepareStatement(DELETE);
-//			pstmt.setInt(1, uDTO.getUid());
-//			int result = pstmt.executeUpdate();
-//			if (result <= 0) {
-//				return false;
-//			}
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//			return false;
-//		} finally {
-//			JDBCUtil.disconnect(pstmt, conn);
-//		}
-//		return true;
-//	}
 }
